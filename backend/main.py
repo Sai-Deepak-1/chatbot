65e84236dbdf1b26fbc4797160d065634be503a7
Sai-Deepak-1 <psaideepak1@gmail.com>
2024-03-28 08:49:52 +0530

    Removed Mangum handler

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers.chat import chat_router
 from app.settings import init_settings
-from mangum import Mangum
 
 load_dotenv()
 
@@ -29,8 +28,6 @@ if environment == "dev":
     )
 
 app.include_router(chat_router, prefix="/api/chat")
-
-handler = Mangum(app=app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
